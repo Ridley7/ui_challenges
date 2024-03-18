8a876cf3d0ee144a85e1236ec8959ed65c06ddb3
Ridley <gonzalezfranks@hotmail.com>
2024-03-18 18:19:24 +0100

    Enrutamiento marcado

---
+++ lib/presentation/animations/animations.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Animations extends StatelessWidget {
+  const Animations({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Animaciones"),),
+      body: ListView(
+        children: [
+          ElevatedButton(
+              onPressed: (){
+                Navigator.pushNamed(context, "/animations/animated_list_one");
+              },
+              child: Text("Animated List One")
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/presentation/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Menu"),),
+      body: ListView(
+        children: [
+          ElevatedButton(
+              onPressed: (){
+                Navigator.pushNamed(context, "/animations");
+              },
+              child: Text("Animaciones")
+          )
+        ],
+      ),
+    );
+  }
+}
