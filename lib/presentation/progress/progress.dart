1456a6d0bba2e9d262966265d00f8bcf9a2ff38b
Ridley <gonzalezfranks@hotmail.com>
2024-04-24 11:45:54 +0200

    Widget de progreso añadidos.

--- lib/presentation/progress/progress.dart
+++ lib/presentation/progress/progress.dart
@@ -5,6 +5,69 @@ class Progress extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder( );
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Progress Indicators"),
+      ),
+
+      body: ProgressView(),
+    );
+  }
+}
+
+class ProgressView extends StatelessWidget {
+  const ProgressView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Column(
+        children: [
+          SizedBox(height: 30,),
+          Text("Progress Indicator"),
+          SizedBox(height: 30,),
+          CircularProgressIndicator(strokeWidth: 2, backgroundColor: Colors.black54,),
+          SizedBox(height: 30,),
+
+          Text("Controlled progress indicator"),
+          SizedBox(height: 30,),
+          ControlledProgressIndicator()
+        ],
+      ),
+    );
+  }
+}
+
+class ControlledProgressIndicator extends StatelessWidget {
+  const ControlledProgressIndicator({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Stream.periodic(const Duration(milliseconds: 300), (value){
+        return (value * 2) / 100;
+      }).takeWhile((value) => value < 100),
+      builder: (context, snapshot) {
+
+        final progressValue = snapshot.data;
+
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Row(
+            children: [
+              CircularProgressIndicator(value: progressValue, strokeWidth: 2,),
+              const SizedBox(width: 20,),
+              Expanded(
+                  child: LinearProgressIndicator(value: progressValue,)
+              )
+            ],
+          ),
+        );
+      }
+    );
   }
 }
